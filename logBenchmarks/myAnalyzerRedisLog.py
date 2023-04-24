8694b1ce11713a09edaa7ac6f9fd8750ea1e4c5a
Angel Motta <angel.motta@utec.edu.pe>
2023-04-24 11:35:40 -0500

    feat: log analyzer first version

---
+++ logBenchmarks/myAnalyzerRedisLog.py
@@ -0,0 +1,21 @@
+import csv
+import itertools
+
+zip_longest = itertools.zip_longest
+
+f1 = open("./1/r1.txt")
+f2 = open("./1/r2.txt")
+
+csv_f1 = csv.reader(f1, delimiter=" ")
+csv_f2 = csv.reader(f2, delimiter=" ")
+
+countDiff = 0
+numLine = 1
+for rowf1, rowf2 in zip_longest(csv_f1, csv_f2):
+    if rowf1[5] != rowf2[5]:
+        countDiff += 1
+        print("Difference at operation #" + str(numLine) + ": " + rowf1[5] + " , " + rowf2[5])
+    numLine += 1
+
+f1.close()
+f2.close()

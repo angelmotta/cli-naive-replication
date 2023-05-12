11b90c437d53dce5d4d3dfe3498d64957b7b93b7
Angel Motta <angel.motta@utec.edu.pe>
2023-05-12 11:20:38 -0500

    feat: refactor send request

--- client/client.go
+++ client/client.go
@@ -55,67 +55,68 @@ func New(idClient uint32, svr1Addr, svr2Addr, svr3Addr string) *Client {
 func (c *Client) CloseLoopClient(wg *sync.WaitGroup, n int) {
 	defer wg.Done() // Decrement the counter when goroutine complete
 
-	log.Println("CloseLoopClient execution started...")
+	log.Printf("ClientId #%v, started CloseLoop...", c.ClientId)
 	for i := 0; i < n; i++ {
-		//log.Printf("iteration #%v from clientId #%v", i, c.ClientId)
-		valPrice := c.GetRandomCurrencyPrice()
-		typeOp := rand.Intn(2) // typeOp: 0 is Set, 1 is Get
-		if typeOp == 0 {
-			log.Printf("ClientId #%v, OpNum #%v: set %v", c.ClientId, i, valPrice)
-			// Writes to Replica1
-			err := c.replica1.SetExchange("usd_pen_", valPrice)
-			if err != nil {
-				log.Panicf("got error Set value opeartion #%v in ExchangeStore: %v", i, err)
-			}
-
-			// Writes to replica2
-			err = c.replica2.SetExchange("usd_pen_", valPrice)
-			if err != nil {
-				log.Panicf("got error Set value %v in ExchangeStore: %v", i, err)
-			}
-
-			// Writes to replica3
-			err = c.replica3.SetExchange("usd_pen_", valPrice)
-			if err != nil {
-				log.Panicf("got error Set value %v in ExchangeStore: %v", i, err)
-			}
-		} else { // typeOp is Get
-			log.Printf("ClientId #%v, OpNum #%v, : get usd_pen_", c.ClientId, i)
-			// Reads from Replica1
-			_, err := c.replica1.GetExchange("usd_pen_")
-			if err != nil {
-				log.Panicf("got error Get value operation #%v in ExchangeStore: %v", i, err)
-			}
-
-			// Reads from Replica2
-			_, err = c.replica2.GetExchange("usd_pen_")
-			if err != nil {
-				log.Panicf("got error Get value operation #%v in ExchangeStore: %v", i, err)
-			}
-
-			// Reads from Replica3
-			_, err = c.replica3.GetExchange("usd_pen_")
-			if err != nil {
-				log.Panicf("got error Get value operation #%v in ExchangeStore: %v", i, err)
-			}
-		}
+		c.sendOneRequest(i)
 	}
-	log.Println("CloseLoopClient execution done")
+	log.Printf("ClientId #%v, finished CloseLoopClient!!", c.ClientId)
 }
 
-func (c *Client) GetRandomCurrencyPrice() string {
+func (c *Client) getRandomCurrencyPrice() string {
 	min := 3708
 	max := 3910
 	n := 8 // Length: 8 bytes
 	// Generate Random
 	valCurrency := float64(rand.Intn(max-min)+min) / 1000
-	//log.Printf("float currency: %v", valCurrency)
 
 	//valPrice := fmt.Sprintf("%f", valCurrency)
 	valPrice := strconv.FormatFloat(valCurrency, 'f', 6, 64)
-	//log.Printf("my string currency: %v", valPrice)
 	if len(valPrice) != n {
 		log.Panicf("got error creating random price value %v in ExchangeStore: this length is not %v bytes", valPrice, n)
 	}
 	return valPrice
+}
+
+func (c *Client) sendOneRequest(i int) {
+	valPrice := c.getRandomCurrencyPrice()
+	typeOp := rand.Intn(2) // typeOp: 0 is Set, 1 is Get
+	if typeOp == 0 {
+		log.Printf("ClientId #%v, OpNum #%v: set %v", c.ClientId, i, valPrice)
+		// Writes to Replica1
+		err := c.replica1.SetExchange("usd_pen_", valPrice)
+		if err != nil {
+			log.Panicf("got error Set value opeartion #%v in ExchangeStore: %v", i, err)
+		}
+
+		// Writes to replica2
+		err = c.replica2.SetExchange("usd_pen_", valPrice)
+		if err != nil {
+			log.Panicf("got error Set value %v in ExchangeStore: %v", i, err)
+		}
+
+		// Writes to replica3
+		err = c.replica3.SetExchange("usd_pen_", valPrice)
+		if err != nil {
+			log.Panicf("got error Set value %v in ExchangeStore: %v", i, err)
+		}
+	} else { // typeOp is Get
+		log.Printf("ClientId #%v, OpNum #%v, : get usd_pen_", c.ClientId, i)
+		// Reads from Replica1
+		_, err := c.replica1.GetExchange("usd_pen_")
+		if err != nil {
+			log.Panicf("got error Get value operation #%v in ExchangeStore: %v", i, err)
+		}
+
+		// Reads from Replica2
+		_, err = c.replica2.GetExchange("usd_pen_")
+		if err != nil {
+			log.Panicf("got error Get value operation #%v in ExchangeStore: %v", i, err)
+		}
+
+		// Reads from Replica3
+		_, err = c.replica3.GetExchange("usd_pen_")
+		if err != nil {
+			log.Panicf("got error Get value operation #%v in ExchangeStore: %v", i, err)
+		}
+	}
 }
